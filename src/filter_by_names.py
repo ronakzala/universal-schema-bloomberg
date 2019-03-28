4318c1ec6258961d2949665b06bcbe43b87c8925
Pallavi Patil <pallavip312@gmail.com>
2019-03-28 12:24:30 -0400

    Add Multiprocessing capability to filtering

--- src/filter_by_names.py
+++ src/filter_by_names.py
@@ -2,67 +2,109 @@ from concrete.util import read_communication_from_file as rcff
 import os
 import concrete.util
 import shutil
+import sys
+import multiprocessing
+
+dir_path = "/mnt/nfs/work1/mccallum/smysore/data/concretely_annotated_nyt/data/comms"
+politicians_file_path = '../data/congressperson_data/unique_congress.txt'
+#path of file which has the names of politicians
 
 
-politiciansFilePath = '../universal-schema-bloomberg/data/congressperson_data/unique_congress.txt' #path of file which has the names of politicians
+def get_politicians_set():
+	'''
+	Read in Unique Politician List and create set object
+	:return set_politicians: Python Set
+	'''
+	set_politicians = set()
 
-setPoliticians = set()
+	with open(politicians_file_path, 'r') as filehandle:
+		filecontents = filehandle.readlines()
+		for line in filecontents:
+			set_politicians.add(line.rstrip())
+			print(line.rstrip())
+			for name in line.split():
+				print(name)
+				set_politicians.add(name)
 
-with open(politiciansFilePath, 'r') as filehandle:  #creating the set of politicians we care about from the corresponding file
-	filecontents = filehandle.readlines()
-	for line in filecontents:
+	return set_politicians
+
+
+def match_article_to_politician(fname, set_politicians):
+	'''
+	Per article function to return a mapping of politician to article
+	- Uses NER tags
+	- Pulls out a tagged person using all consecutive 'PERSON' tags
+	:param fname: Current file being filtered
+	:param set_politicians: Set of relevant politicians
+	:return filtered_dictionary: politician name-list of filenames
+	'''
+	fname = fname.strip()
+	full_path = os.path.join(dir_path, fname)
+	comm =  rcff(dir_path)
+	filtered_dictionary = {}
+	count = 0
+
+	for (uuid, tokenization_object) in comm.tokenizationForUUID.items():
+		#iterating through tokenization objects for one comm file
+		print("Count : ", count + 1)
+	
+		#picking up the NER tagged token list
+		tagged_token_list_object = None
+		for tto in (tokenization_object.tokenTaggingList):
+			if(tto.taggingType == 'NER'):
+				tagged_token_list_object = tto.taggedTokenList
+				break
+
+		for idx in range(len(tokenization_object.tokenList)):
+			#iterating throught the token list to find all the 'PERSON' tags
+			token_object = tokenization_object.tokenList[idx]	
+			#token object
+			tagged_token_object = tagged_token_list_object[token_object.tokenIndex]
+			#tagged token Object from the NER tagged token List
+
+			if(tagged_token_object.tag == 'PERSON'):
+				person = ''
+				while(tagged_token_object.tag == 'PERSON'):
+					#if the cuurent token was a 'PERSON' add to person for all continuous 'PERSON' tags
+					person += token_object.text
+					idx += 1
+					token_object = tokenization_object.tokenList[idx]
+					tagged_token_object = tagged_token_list_object[token_object.tokenIndex]
+
+				if(person in set_politicians):
+					#check if this person is in the politician set, add to final dictionary if true
+					if(person in filtered_dictionary):
+						filtered_dictionary[person].append(filename) 
+					else:
+						#tempList = []
+						#tempList[0] = filename
+						filtered_dictionary[person] = [filename] 
 		count += 1
-		setPoliticians.add(line.rstrip())
-		print(line.rstrip())
-		for name in line.split():
-			print(name)
-			setPoliticians.add(name)
+
+		return filtered_dictionary
 
 
-path = '../data/anyt_sample/AnnotatedNYT-1001191.comm' #TODO : replace by path of directory and add loop to iterate over all the files.
+def get_filtered_files():
+	'''
+	Use multiprocessing to filter files containing relevant politicians.
+	- Currently uses NER tags, no desk filtering
+	- Print out counts of filtered politician articles
+	:return None, dump data to file 
+	'''
+	names = open("../data/anyt/anyt_filenames.txt", 'r')
+	process_pool = mp.Pool(processes=mp.cpu_count(), maxtasksperchild=10000)
+	poltician_set = get_politician_set()
+	filtered_articles = {politician: [] for politician in politician_set}
+
+	for politician_article_dicts in process_pool.imap_unordered(match_article_to_politician, names, chunksize=mp.cpu_count()):
+		for k, v in politician_article_dicts.items():
+			filtered_articles[k].extend(v)
+	process_pool.close()
+	process_pool.join()
+	names.close()
+	print(filtered_articles)
 
 
-comm =  rcff(path)
-
-filteredDictionary = {} #final dictionary - key. : politician name, value : list of filenames
-
-count = 0
-for (uuid,tokenizationObject) in comm.tokenizationForUUID.items(): #iterating through tokenization objects for one comm file
-	
-	print("Count : ", count+1)
-	
-	#picking up the NER tagged token list
-	taggedTokenListObject = None		#
-	for tto in (tokenizationObject.tokenTaggingList):
-		if(tto.taggingType == 'NER'):
-			taggedTokenListObject = tto.taggedTokenList
-			break
-
-	for idx in range(len(tokenizationObject.tokenList)): #iterating throught the token list to find all the 'PERSON' tags
-
-		tokenObject = tokenizationObject.tokenList[idx]		#token object
-		taggedTokenObject = taggedTokenListObject[tokenObject.tokenIndex] 		#tagged token Object from the NER tagged token List
-
-		if(taggedTokenObject.tag == 'PERSON'):
-			
-			person = ''
-			while(taggedTokenObject.tag == 'PERSON'):	#if the cuurent token was a 'PERSON' add the text to person variable, continue doing this for all continuous 'PERSON' tags
-				person += tokenObject.text
-				idx = idx+1
-				tokenObject = tokenizationObject.tokenList[idx]
-				taggedTokenObject = taggedTokenListObject[tokenObject.tokenIndex]
-
-
-			if(person in setPoliticians): 	#check if this person is in the politician set, add to final dictionary if true
-				if(person in filteredDictionary):
-					filteredDictionary[person].append(filename) 
-				else:
-					tempList = []
-					tempList[0] = filename
-					filteredDictionary[person] = tempList 
-
-	count = count + 1
-
-
-	#TODO : store the dictionary
-	#Combine this with filter by desk ???
+get_filtered_files()
+#TODO : store the dictionary
+#Combine this with filter by desk ???
