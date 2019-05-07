8394c7832f406afdc6bc0623dc1a5e7daabd96e1
Pallavi Patil <pallavip312@gmail.com>
2019-05-07 17:04:12 -0400

    Add text model run-script for Kriti's reference

--- src/run-text-model.sh
+++ src/run-text-model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=train_roll_call_106_0.1
+#SBATCH --job-name=text_106
 #SBATCH --output=log_files/text_model_106.txt  # output file
 #SBATCH -e log_files/text_model_106.err        # File to which STDERR will be written
 #SBATCH --partition=1080ti-long # Partition to submit to 
@@ -10,4 +10,11 @@
 #SBATCH --mem-per-cpu=4000    # Memory in MB per cpu allocated
 
 module add cuda90/toolkit/9.0.176
-python text_model.py --datafile ../data/106_no_eval.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106
+python text_model.py --datafile ../data/106_no_eval.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --lognum 1
+python text_model.py --datafile ../data/106_no_eval.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --lognum 2
+python text_model.py --datafile ../data/106_no_eval.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --lognum 3
+python text_model.py --datafile ../data/106_no_eval.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --lognum 4
+python text_model.py --datafile ../data/106.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --runeval True --lognum 1
+python text_model.py --datafile ../data/106.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --runeval True --lognum 2
+python text_model.py --datafile ../data/106.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --runeval True --lognum 3
+python text_model.py --datafile ../data/106.hdf5 --textfile ../data/106_text_bag.hdf5 --classifier nn_embed_m_nocv --eta 0.1 --nepochs 10 --dp 10 --modeltype bag --congress 106 --runeval True --lognum 4
